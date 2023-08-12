395efb54ab38153ab7303edc034d1344b4f9ad66
Ruslan Volovenko <vokoloven@gmail.com>
2023-08-12 14:23:05 +0300

    feat: added card for rendering content on mainpage

---
+++ src/components/ui/Cards/HomeCard.tsx
@@ -0,0 +1,57 @@
+import { useSelector } from 'react-redux';
+import { Box, Paper, Typography, Skeleton } from '@mui/material';
+import { capitalize } from 'utils/capitalize';
+import { selectService } from 'redux/selectors';
+
+export const HomeCard = () => {
+    const {
+        loading,
+        data: { data }
+    } = useSelector(selectService);
+
+    const items: {
+        name: string;
+        field: string;
+    }[] = [
+        { name: 'status', field: data?.[0].Status as string },
+        { name: 'sender', field: data?.[0].WarehouseSender as string },
+        { name: 'receiver', field: data?.[0].WarehouseRecipient as string }
+    ];
+
+    return loading !== 'idle' ? (
+        <Paper sx={{ mt: 4, p: 2 }} elevation={4}>
+            <Box
+                component={'ul'}
+                sx={{
+                    p: 0,
+                    m: 0
+                }}
+            >
+                {items.map(({ name, field }) => (
+                    <Box
+                        component={'li'}
+                        key={name}
+                        sx={{
+                            listStyle: 'none',
+                            '&:not(:last-child)': {
+                                mb: 1
+                            }
+                        }}
+                    >
+                        <Typography
+                            variant={'h6'}
+                            component={'p'}
+                            sx={{ fontSize: 16 }}
+                        >
+                            {loading === 'pending' ? (
+                                <Skeleton />
+                            ) : (
+                                `${capitalize(name)}: ${field} `
+                            )}
+                        </Typography>
+                    </Box>
+                ))}
+            </Box>
+        </Paper>
+    ) : null;
+};
